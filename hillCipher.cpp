3b2496e4b6f2c833fbc70f6660b0b030ed080f85
Binish Manandhar <binish784@hotmail.com>
2018-06-04 19:59:19 +0545

    Initial commit

--- hillCipher.cpp
+++ hillCipher.cpp
@@ -1,3 +1,12 @@
+
+//*********************************************
+
+// This is the hill climb implementation
+
+// This program takes  2 X 2 matrix key , converts the plain_text into upper case
+
+//*********************************************
+
 #include<iostream>
 #include<string.h>
 
@@ -16,23 +25,30 @@ int main(){
 		}
 	}
 	
+	cout<<"\nEnter Plain Text:";
+	cin>>plain_text;
+	for(i=0;i<strlen(plain_text);i++){
+		if(int(plain_text[i])>=97 && int(plain_text[i])<=122){
+			plain_text[i]=char(int(plain_text[i])-32);
+		}
+	}
+	cout<<"\nEntered Plain Text : "<<plain_text;
+	
+	
 	inverse[1][1]=key[0][0];
 	inverse[0][0]=key[1][1];
 	inverse[0][1]=-key[0][1]+26;
 	inverse[1][0]=-key[1][0]+26;
 
-	cout<<"inverse Matrix:\n";
+	cout<<"\n\nCalulated Inverse Matrix:\n\n";
 	for(i=0;i<2;i++){
 		for(j=0;j<2;j++){
-			cout<<inverse[i][j]<<"\t";			
+			cout<<"\t"<<inverse[i][j];			
 		}
 		cout<<"\n";
 	}
 	
-
 	
-	cout<<"Enter Plain Text:";
-	cin>>plain_text;
 	cout<<"\n";
 	charCount=(strlen(plain_text));
 	if((charCount%2)!=0){
@@ -40,7 +56,7 @@ int main(){
 		charCount+=1;
 	}
 	
-	cout<<"\nEncryption:\n";
+	cout<<"\nEncrypted Message: \t";
 	
 	for(i=0;i<charCount;i=i+2){
 		x=int(plain_text[i])%65;
@@ -55,9 +71,10 @@ int main(){
 	}
 	
 	
-	cout<<"\nDecryption:\n";
+	cout<<"\n\nDecryption Text :\t";
 	
 	for(i=0;i<charCount;i=i+2){
+		x=int(cipher[i])%65;
 		y=int(cipher[i+1])%65;
 		Ex=(x*inverse[0][0]+y*inverse[1][0])%26;
 		Ey=(x*inverse[0][1]+y*inverse[1][1])%26;
